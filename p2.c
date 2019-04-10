70aef1bde2b923570837c2f1fcbe4180324a1b14
Punit <puneetdevaramani@gmail.com>
2019-04-10 17:03:05 +0530

    Update p2.c

--- p2.c
+++ p2.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 int main()
 {
-    int i, j, rows;
+    int i, j, rows=6;
 
-    printf("Enter number of rows: ");
-    scanf("%d",&rows);
-
+   
     for(i=1; i<=rows; ++i)
     {
         for(j=1; j<=i; ++j)
